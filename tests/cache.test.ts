13f0e2502cc250d605b0e43d253b3b96da50aa55
Hidetake Iwata <int128@gmail.com>
2021-10-02 14:57:00 +0900

    Add tests (#5)

--- tests/cache.test.ts
+++ tests/cache.test.ts
@@ -24,3 +24,63 @@ test('on pull request', () => {
     to: null,
   })
 })
+
+test('on push branch', () => {
+  const c = cache.infer(
+    {
+      eventName: 'push',
+      ref: 'refs/heads/main',
+      payload: {},
+    },
+    {
+      image: 'ghcr.io/int128/sandbox/cache',
+      tagPrefix: '',
+    }
+  )
+  expect(c).toStrictEqual({
+    from: 'ghcr.io/int128/sandbox/cache:main',
+    to: 'ghcr.io/int128/sandbox/cache:main',
+  })
+})
+
+test('on push tag', () => {
+  const c = cache.infer(
+    {
+      eventName: 'push',
+      ref: 'refs/tags/v1.0.0',
+      payload: {
+        repository: {
+          name: 'sandbox',
+          owner: { login: 'int128' },
+          default_branch: 'main',
+        },
+      },
+    },
+    {
+      image: 'ghcr.io/int128/sandbox/cache',
+      tagPrefix: '',
+    }
+  )
+  expect(c).toStrictEqual({
+    from: 'ghcr.io/int128/sandbox/cache:main',
+    to: null,
+  })
+})
+
+test('on schedule', () => {
+  const c = cache.infer(
+    {
+      eventName: 'schedule',
+      ref: 'refs/heads/main',
+      payload: {},
+    },
+    {
+      image: 'ghcr.io/int128/sandbox/cache',
+      tagPrefix: '',
+    }
+  )
+  expect(c).toStrictEqual({
+    from: 'ghcr.io/int128/sandbox/cache:main',
+    to: null,
+  })
+})
