52d4b8764ad417c6f7bd83f1b2e8341dc4731ceb
Hidetake Iwata <int128@gmail.com>
2023-09-26 10:30:57 +0900

    Support `extra-cache-from` and `extra-cache-to` (#587)
    * Support `extra-cache-from` and `extra-cache-to`
    
    * Fix example
    
    * Show log

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,11 @@ const main = async (): Promise<void> => {
     flavor: core.getMultilineInput('flavor'),
     tagPrefix: core.getInput('tag-prefix'),
     tagSuffix: core.getInput('tag-suffix'),
+    extraCacheFrom: core.getInput('extra-cache-from'),
+    extraCacheTo: core.getInput('extra-cache-to'),
     token: core.getInput('token', { required: true }),
   })
+  core.info(`Setting outputs: ${JSON.stringify(outputs, undefined, 2)}`)
   core.setOutput('cache-from', outputs.cacheFrom)
   core.setOutput('cache-to', outputs.cacheTo)
 }
